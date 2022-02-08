29d98792422e68cc460d8800d240d927f72514f9
priscila <2109148@upy.edu.mx>
2022-02-08 08:35:39 -0600

    added inputsMain

---
+++ Unit1/inputsMain.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+char* myName = "Priscila"; //%s
+//atoi(anStringHere): //atof
+//char*  str ="";
+//int aNumber = 18;
+
+int main(int argc, char** argv){
+    //sprintf(str, "%d", aNumber);
+    printf("Wlcome to program %s\n", argv [0]);
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    int sum = num1 + num2;
+    printf("The sum is equal to %d\n", sum);
+
+    return 0;
+}
+
